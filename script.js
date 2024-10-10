f758bf8998ee60dd77cbac3ebbae8d43d0d04172
Emre Soybas <emresoybas2023@gmail.com>
2024-10-10 17:22:02 -0500

    build phase timeout implemented

--- script.js
+++ script.js
@@ -1,15 +1,39 @@
-const button = document.querySelector('#button');
-const button1 = document.querySelector('#old');
+let directionX = 1;
+let directionY = 1;
 
-const jsConfetti = new JSConfetti();
+let windowHeight = window.innerHeight;
+let windowWidth = window.innerWidth;
 
-button.addEventListener('click', () => {
-    jsConfetti.addConfetti({
-        emojis: ['👵'],
-    });
+let message = document.getElementById("tempMsg");
+
+function edgeDetector(){
+    let rect = message.getBoundingClientRect();
+    if (rect.left <= 0 || rect.right >= windowWidth){
+        directionX *= -1;
+    }
+    if (rect.top <= 0 || rect.bottom >= windowHeight){
+        directionY *= -1;
+    }
+}
+
+function float(){
+    let currentLeft = parseFloat(message.style.left) || 0;
+    let currentTop = parseFloat(message.style.top) || 0;
+    
+    message.style.left = (currentLeft + directionX) + 'px';
+    message.style.top = (currentTop + directionY) + 'px';
+}
+function loop() {
+    float();
+    edgeDetector();
+    requestAnimationFrame(loop);
+}
+
+window.addEventListener('resize', function() {
+    windowHeight = window.innerHeight;
+    windowWidth = window.innerWidth;
+    message.style.left = '50px';
+    message.style.top = '50px';
 });
-button1.addEventListener('click', () => {
-    jsConfetti.addConfetti({
-        emojis: ['👵'],
-    });
-});
+
+loop();
