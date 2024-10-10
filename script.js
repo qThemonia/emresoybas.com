478b956e394daed2aaea3cfc29a7ca66f0723361
Emre Soybas <emresoybas2023@gmail.com>
2024-10-10 16:16:30 -0500

    Light Testing

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
-const button = document.querySelector('#button');
-const button1 = document.querySelector('#old');
+import * as THREE from 'three';
 
-const jsConfetti = new JSConfetti();
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
 
-button.addEventListener('click', () => {
-    jsConfetti.addConfetti({
-        emojis: ['👵'],
-    });
-});
-button1.addEventListener('click', () => {
-    jsConfetti.addConfetti({
-        emojis: ['👵'],
-    });
-});
+const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 500 );
+camera.position.set( 0, 0, 10 );
+camera.lookAt( 0, 0, 0 );
+
+const scene = new THREE.Scene();
+
+
+// geometry
+const geometry = new THREE.BoxGeometry( 1, 1, 1);
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+
+// line
+const cube = new THREE.Mesh( geometry, material);
+scene.add( cube );
+
+const light = new THREE.PointLight (0xffffff, 1, 100);
+light.position.set(0, 0, 0);
+light.castShadow = true;
+scene.add(light);
+
+function animate() {
+    cube.rotation.y += 0.01;
+    cube.rotation.x += 0.01;
+	renderer.render( scene, camera );
+}
+renderer.setAnimationLoop( animate );
